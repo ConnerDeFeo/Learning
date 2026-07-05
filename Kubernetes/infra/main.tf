f9af331d1f1e956456e8a08db56c36b4bf9917e0
ConnerDeFeo <ninjanerozz@gmail.com>
2026-07-05 16:39:34 -0400

    M24

--- Kubernetes/infra/main.tf
+++ Kubernetes/infra/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.0"
+    }
   }
 }
 
@@ -38,6 +42,8 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
+  enable_irsa = true
+
   cluster_name    = "k8s-course"
   cluster_version = "1.30"
 
@@ -57,6 +63,27 @@ module "eks" {
   }
 }
 
+module "ebs_csi_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.0"
+
+  role_name             = "ebs-csi-driver-k8s-course"
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
+
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name             = module.eks.cluster_name
+  addon_name                = "aws-ebs-csi-driver"
+  service_account_role_arn  = module.ebs_csi_irsa.iam_role_arn
+}
+
 resource "aws_ecr_repository" "app" {
   name                 = "k8s-course-app"
   image_tag_mutability = "MUTABLE"
@@ -64,6 +91,40 @@ resource "aws_ecr_repository" "app" {
   image_scanning_configuration {
     scan_on_push = true
   }
+}
+
+# Adding load balancer
+data "http" "alb_iam_policy" {
+  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json"
+}
+
+resource "aws_iam_policy" "alb_controller" {
+  name   = "AWSLoadBalancerControllerIAMPolicy-k8s-course"
+  policy = data.http.alb_iam_policy.response_body
+}
+
+module "alb_controller_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.0"
+
+  role_name = "alb-controller-k8s-course"
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "alb_controller" {
+  role       = module.alb_controller_irsa.iam_role_name
+  policy_arn = aws_iam_policy.alb_controller.arn
+}
+
+# Outputs
+output "alb_controller_role_arn" {
+  value = module.alb_controller_irsa.iam_role_arn
 }
 
 output "ecr_repository_url" {
